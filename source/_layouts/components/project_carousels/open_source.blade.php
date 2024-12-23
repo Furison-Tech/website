45f91a85939e3276277a6f079fd436915dc71e90
sean <seanscholte15@outlook.com>
2024-12-23 16:54:13 +0100

    added opensource projects

--- source/_layouts/components/project_carousels/open_source.blade.php
+++ source/_layouts/components/project_carousels/open_source.blade.php
@@ -1,14 +1,35 @@
 <div class="flex gap-2 py-2 items-center trans-mt-500ms">
     <div class="flex-none">
         <img width="40" height="40" loading="lazy" class="rounded-md"
-             src="/assets/images/appicons/appicon_placeholder.jpg" alt="tailglass logo">
+             src="/assets/images/appicons/editor-js-audio-logo-640.jpg" alt="tailglass logo">
     </div>
     <div class="grow font-normal text-white">
         <span class="block md:hidden lg:block">
-            Tailglass (in development)
+            EditorJs Audio (released)
         </span>
         <span class="opacity-50 hidden md:block lg:hidden cursor-not-allowed">
-            Tailglass (in development)
+            EditorJs Audio (released)
+        </span>
+    </div>
+    <a href="https://github.com/Furison-Tech/editorjs-audio" class="inline-block md:hidden lg:inline-block
+    flex-none code-font text-white underline decoration-sky-500 underline-offset-4">
+        Read More
+    </a>
+</div>
+
+<hr class="opacity-50">
+
+<div class="flex gap-2 py-2 items-center trans-mt-500ms">
+    <div class="flex-none">
+        <img width="40" height="40" loading="lazy" class="rounded-md"
+             src="/assets/images/appicons/laraveditorjs-logo-640.jpg" alt="tailglass logo">
+    </div>
+    <div class="grow font-normal text-white">
+        <span class="block md:hidden lg:block">
+            Laraveditorjs (in development)
+        </span>
+        <span class="opacity-50 hidden md:block lg:hidden cursor-not-allowed">
+            Laraveditorjs (in development)
         </span>
     </div>
     <span class="inline-block md:hidden lg:inline-block cursor-not-allowed
